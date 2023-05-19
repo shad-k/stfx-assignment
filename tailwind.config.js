d35cad6b2852ae068601c8022a25175e8096d482
Shadab Khan <shadab.2005@gmail.com>
2023-05-19 20:28:10 +0530

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      gray: {
+        ...colors.gray,
+        550: "#585A5F",
+      },
+    },
+    extend: {
+      colors: {
+        "primary-green": "#00CC6A",
+        "accent-yellow": "FFC700",
+        field: "#1B1F28",
+        section: "#10131B",
+        "base-100": "#1e1e1e"
+      },
+    },
+  },
+  plugins: [],
+}
+
